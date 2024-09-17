0ecdaa07ab4e7439fe20ea0a7b47628a73c00899
juan <juanpo.alvarado@gmail.com>
2024-09-17 14:55:23 -0600

    update

---
+++ service1/test_app.py
@@ -0,0 +1,16 @@
+import unittest
+from app import app
+
+class TestService1(unittest.TestCase):
+    def setUp(self):
+        self.client = app.test_client()
+
+    def test_hello(self):
+        response = self.client.get('/')
+        data = response.get_json()
+        self.assertEqual(response.status_code, 200)
+        self.assertIn('message', data)
+        self.assertIn('Hello from', data['message'])
+
+if __name__ == '__main__':
+    unittest.main()
